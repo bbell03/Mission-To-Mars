52952e3070ba8c9d6f256ddb6f1356b1b71c0744
Brandon Bell <brandonbell@apples-MacBook-Air.local>
2021-07-31 20:42:34 -0400

    pictures working

--- scraping.py
+++ scraping.py
@@ -76,7 +76,7 @@ def mars_facts():
     #Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
-def hemispheres():
+def hemispheres_scrape(browser):
     # 1. Use browser to visit the URL
     url = 'https://marshemispheres.com/'
 
@@ -93,8 +93,8 @@ def hemispheres():
     hemisphere_imgs = img_soup.find_all('img', class_='thumb')
 
     for i in range(len(hemisphere_imgs)):
-        hemisphere_image_urls.append({'img_url':hemisphere_imgs[i]['src']})
-        hemisphere_image_urls.append({'title':hemisphere_titles[i].text})
+        hemisphere_image_urls.append({'img_url':hemisphere_imgs[i]['src'],
+                                      'title':hemisphere_titles[i].text})
 
     return hemisphere_image_urls
 
@@ -103,14 +103,14 @@ def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
     news_title, news_paragraph = mars_news(browser)
-
     #Run all scraping functions and store results in dictionary
     data = {
       "news_title": news_title,
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemispheres": hemispheres_scrape(browser)
     }
 
     # Stop webdriver and return dataframe
